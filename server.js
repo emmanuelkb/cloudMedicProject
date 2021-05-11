68b7c0208637acefe7f33738c2f47aabc9dd708c
KB <kaabutu@gmail.com>
2021-05-11 14:05:51 +0000

    Version 1.0.0

--- server.js
+++ server.js
@@ -77,6 +77,9 @@ app.use("/message", messageRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
