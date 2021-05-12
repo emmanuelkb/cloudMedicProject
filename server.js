16dff655a69d1e27a51c8d4cb843831ef0ff0132
KB <kaabutu@gmail.com>
2021-05-12 10:31:49 +0000

    version 1.0.0

--- server.js
+++ server.js
@@ -85,6 +85,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.use("/user", userRouter);
 app.use("/medic", medicRouter);
 app.use("/conversation", conversationRouter);
