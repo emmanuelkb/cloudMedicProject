7dc17a3b554042d857f28aecf4742f90637f0512
KB <kaabutu@gmail.com>
2021-05-12 10:33:13 +0000

    version 1.0.0

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 app.use("/user", userRouter);
