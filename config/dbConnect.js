16e3c0f2adc260313ee2ba4c80a0c83d4e4a458e
KB <kaabutu@gmail.com>
2021-05-12 16:11:26 +0000

    version 1.0.0

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+MONGODB_URI =
+  "mongodb+srv://emmanuelkb:ekwab@2010@dev.vzwvi.mongodb.net/cloudmedic?retryWrites=true&w=majority";
+
 mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/cloudmedic", {
     useCreateIndex: true,
