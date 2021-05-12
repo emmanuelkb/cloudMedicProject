4334a6b351caa64b3bd03c9f80e088f0cd52922d
KB <kaabutu@gmail.com>
2021-05-12 16:20:58 +0000

    version 1.0.0

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://emmanuelkb:ekwab@2010@dev.vzwvi.mongodb.net/cloudmedic?retryWrites=true&w=majority",
-    {
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/cloudmedic", {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Database connected successfully"))
   .catch((err) => console.log(err.message));
