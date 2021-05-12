5095562e5281caee774b2f2bff038c41dfbbe60e
KB <kaabutu@gmail.com>
2021-05-12 09:57:19 +0000

    Version 1.0.0

--- server.js
+++ server.js
@@ -82,12 +82,9 @@ app.use("/message", messageRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  // });
-  app.get(/^\/(?!user).*/, (req, res) => {
-    // don't serve api routes to react app
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  app.get("*", (req, res) => {
+    // res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
 }
 
